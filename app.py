dea7565d7d6e914d0fe0bd9d752342f7e37a6cc7
Brienna Herold <brienna.herold@gmail.com>
2016-04-30 14:37:17 -0400

    Improve comments

--- app.py
+++ app.py
@@ -8,65 +8,67 @@ import os
 from werkzeug import secure_filename
 import flask.ext.login as flask_login
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SECRET_KEY'] = 'important to keep unknown in production' # for form
-app.config['UPLOAD_FOLDER'] = 'static/caps/'
-app.config['ALLOWED_EXTENSIONS'] = set(['png'])
-db = SQLAlchemy(app)  # creates db instance and binds it to the app
-# set up the login manager by instantiating it and telling it about our Flask app
+# Initialize the Flask application and set configurations
+bottlecapp = Flask(__name__)
+bottlecapp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+bottlecapp.config['SECRET_KEY'] = 'important to keep unknown in production' # for form
+bottlecapp.config['UPLOAD_FOLDER'] = 'static/caps/'
+bottlecapp.config['ALLOWED_EXTENSIONS'] = set(['png'])
+
+# create db instance and bind it to the app
+db = SQLAlchemy(bottlecapp)  
+
+# set up the login manager by instantiating it and telling it about the app
 login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(bottlecapp)
 
 
 class User(db.Model):
-    __tablename__ = 'user'
+	'''A class definition for a user object.
+	'''
+	id = db.Column(db.Integer, primary_key=True)
+	# User authentication information
+	username = db.Column(db.String)
+	password = db.Column(db.String)
+	authenticated = db.Column(db.Boolean, default=False)
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String)
-    password = db.Column(db.String)
-    authenticated = db.Column(db.Boolean, default=False)
+	# define one to many relationship with child table
+	caps = db.relationship('Cap', backref='user', lazy='dynamic')
 
-    # define one to many relationship with child table
-    caps = db.relationship('Cap', backref='user', lazy='dynamic')
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
 
-    def __init__(self, username, password):
-    	self.username = username
-    	self.password = password
+	def __repr__(self):
+		return '<User {0}, {1}>'.format(self.username, self.password)
 
-    def __repr__(self):
-    	return '<User {0}, {1}>'.format(self.username, self.password)
+	def is_active(self):
+		'''True, as all users are active.'''
+		return True
 
-    def is_active(self):
-        """True, as all users are active."""
-        return True
+	def get_id(self):
+		'''Return the id to satisfy Flask-Login's requirements.'''
+		return self.id
 
-    def get_id(self):
-        """Return the id to satisfy Flask-Login's requirements."""
-        return self.id
+	def is_authenticated(self):
+		'''Return True if the user is authenticated (logged in).'''
+		return self.authenticated
 
-    def is_authenticated(self):
-        """Return True if the user is authenticated."""
-        return self.authenticated
-
-    def is_anonymous(self):
-        """False, as anonymous users aren't supported."""
-        return False
-
-    def get_username(self):
-    	return self.username
+	def is_anonymous(self):
+		'''False, as anonymous users aren't supported.'''
+		return False
 
 
 @login_manager.user_loader
-def load_user(id):
-	print('user loader:', User.query.get(id))
-	return User.query.get(id)
+def load_user(user_id):
+	'''Loads user object from database, using the user id stored in the session.'''
+	return User.query.get(user_id)
 
 
-@app.route("/", methods=["GET", "POST"])
+@bottlecapp.route('/', methods=['GET', 'POST'])
 def login():
-    """For GET requests, display the login form. For POSTS, login the current user
-    by processing the form."""
+    '''For GET requests, display the login form. For POSTS, login the current user
+    by processing the form.'''
     users = User.query.all()
     form = LoginForm()
     if form.validate_on_submit():
@@ -78,22 +80,23 @@ def login():
             	db.session.add(user)
             	db.session.commit()
             	flask_login.login_user(user, remember=True)
-            	return redirect(url_for("gallery"))
+            	return redirect(url_for('gallery'))
             else:
+            	# Incorrect password, flash message
             	flash('incorrect password')
         else:
-        	print('new user')
-        	flash('New user')
+        	# New user, flash message
+        	flash('New user, please sign up')
     else:
     	flash('Please log in.')
     button = 'Log in'
-    return render_template("login.html", form=form, users=users, button=button)
+    return render_template('login.html', form=form, users=users, button=button)
 
 
-@app.route("/logout", methods=["GET"])
+@bottlecapp.route('/logout', methods=['GET'])
 @flask_login.login_required
 def logout():
-    """Logout the current user."""
+    '''Logout the current user.'''
     user = flask_login.current_user
     user.authenticated = False
     db.session.add(user)
@@ -102,7 +105,7 @@ def logout():
     return redirect(url_for('login'))
 
 
-@app.route('/signup', methods=["GET", "POST"])
+@bottlecapp.route('/signup', methods=['GET', 'POST'])
 def signup():
 	form = LoginForm()
 	users = User.query.all()
@@ -116,7 +119,7 @@ def signup():
 			db.session.add(User(username, password))
 			db.session.commit()
 			# create new user folder
-			newpath = app.config['UPLOAD_FOLDER'] + username
+			newpath = bottlecapp.config['UPLOAD_FOLDER'] + username
 			if not os.path.exists(newpath):
 				os.makedirs(newpath)
 			return redirect(url_for('login'))
@@ -126,12 +129,12 @@ def signup():
 
 
 class LoginForm(Form):
-    """A class definition for the login form object.
+    '''A class definition for the login form object.
 
     Attributes:
         username (object)
         password (object)
-    """
+    '''
     username = StringField('username:', validators=[DataRequired()])
     password = StringField('password:', validators=[DataRequired()])
 
@@ -154,21 +157,19 @@ class Cap(db.Model):
 		return '<Cap {0}, {1}, {2}>'.format(self.date, self.path, self.user_id)
 
 	def allowed_ext(filename):
-		"""For a given file, return whether or not it's an allowed type."""
-		return filename.rsplit('.')[1] in app.config['ALLOWED_EXTENSIONS']
+		'''For a given file, return whether or not it's an allowed type.'''
+		return filename.rsplit('.')[1] in bottlecapp.config['ALLOWED_EXTENSIONS']
 
 	def add():
-		"""Add cap filepath to database if its filepath is not already in database"""
-		username = flask_login.current_user.get_username()
+		'''Add cap to database if it has not already been added'''
+		username = flask_login.current_user.username
 		user_id = flask_login.current_user.get_id()
-		caps_dir = app.config['UPLOAD_FOLDER'] + username + '/'
+		caps_dir = bottlecapp.config['UPLOAD_FOLDER'] + username + '/'
 		for file in os.listdir(caps_dir):
-			print('file:', file)
 			path = caps_dir + os.path.relpath(file)  # note: use relpath to later accommodate user folders
 			date = os.path.getctime(path)
 			ext = os.path.splitext(file)[1]
-			if ext == '.jpg' or ext == '.jpeg' or ext == '.png':
-				print('yes')
+			if ext == '.png':
 				if db.session.query(Cap.path).filter_by(path=path).scalar() is None:
 					db.session.add(Cap(date, path, user_id))
 					db.session.commit()
@@ -181,34 +182,34 @@ class CapSchema(Schema):
 	user_id = fields.Int()
 
 
-@app.route('/gallery', methods=('GET', 'POST'))
+@bottlecapp.route('/gallery', methods=('GET', 'POST'))
 @flask_login.login_required
 def gallery():
-	Cap.add() # if not already in database
+	Cap.add()
 	current_user = User.query.get(flask_login.current_user.get_id())
 	thumbnails = current_user.caps.all()
 	schema = CapSchema(many=True)
 	thumbnails_json = schema.dumps(thumbnails)
 	caps = Markup(thumbnails_json.data) # safer than {{ caps|tojson }} at keeping format as json while passing from jinja to js
-	users = User.query.all()
-	return render_template('grid.html', caps=caps, users=users)
+	return render_template('grid.html', caps=caps, users=User.query.all())
 
 
-@app.route('/upload')
+@bottlecapp.route('/upload')
 @flask_login.login_required
 def upload():
 	return render_template('upload.html')
 
 
-@app.route('/uploaded', methods=('GET', 'POST'))
+@bottlecapp.route('/uploaded', methods=('GET', 'POST'))
 @flask_login.login_required
 def uploaded():
-	username = flask_login.current_user.get_username()
+	'''If uploaded cap file's extension is allowed, save to user folder.'''
 	if request.method == 'POST':
 		cap = request.files['file']
 		if cap and Cap.allowed_ext(cap.filename):
 			cap_secure = secure_filename(cap.filename)
-			cap.save(os.path.join(app.config['UPLOAD_FOLDER'] + username, cap_secure))
+			username = flask_login.current_user.username
+			cap.save(os.path.join(bottlecapp.config['UPLOAD_FOLDER'] + username, cap_secure))
 			return redirect(url_for('gallery'))
 		else:
 			flash('Incorrect file type. Please use png.')
@@ -217,7 +218,7 @@ def uploaded():
 
 if __name__ == '__main__':
 	db.create_all()
-	app.run(debug=True)
+	bottlecapp.run(debug=True)
 
 
 
