e24624196e7dd1961ab500f7aeca0e8faa65a84f
Brienna Herold <brienna.herold@gmail.com>
2016-05-29 19:31:10 -0400

    Remove force layout, implement simpler layout

--- static/interactivity.js
+++ static/interactivity.js
@@ -5,8 +5,6 @@ var data = caps;
 // Set variables
 var width = window.innerWidth;
 var height = window.innerHeight;
-var preloaded = false;  // caps have not been preloaded yet
-var caps = {};  // where preloaded caps will be stored
 
 // Select and configure the canvas element
 var canvas = d3.select("canvas")
@@ -14,35 +12,16 @@ var canvas = d3.select("canvas")
   .attr("height", height)
   // Preload cap images for later use
   .call(preloadCaps)
-  .node();
+  .node()
 
 // Get the canvas context to access drawing interface
 var context = canvas.getContext("2d");
 
-// Create and configure the force layout
-var force = d3.layout.force()
-  .size([width, height])
-  .nodes(caps)
-  .charge(1)
-  .start();
-
-force.on('tick', function() {
-  if (preloaded) {
-    // Clear the canvas
-    context.clearRect(0, 0, width, height);
-
-    for (var cap in caps) {
-      x = Math.random() * 200;
-      y = Math.random() * 200;
-      context.drawImage(caps[cap], x, y, 50, 50);
-
-    }
-  }
-});
-
 function preloadCaps() {
+  var caps = {};  // dict needed to later store info about each cap
   var loadedCaps = 0;
   var numCaps = data.length;
+
   for (var i = 0; i < data.length; i++) {
     // Create a new img element
     caps[i] = new Image();
@@ -50,13 +29,26 @@ function preloadCaps() {
     caps[i].addEventListener("load", function() {
       loadedCaps++;
       if (loadedCaps >= numCaps) {
-        // Finish preloading
-        preloaded = true;
-        console.log('Caps have been preloaded.');
+        // Finish preloading, draw caps
+        drawCaps(caps);
       }
     });
     // Set source url of image
     caps[i].src = data[i].path;
+  }
+}
+
+/** Draw loaded caps onto the canvas */
+function drawCaps(caps) {
+  var x = 0;
+  var y = 0;
+  for (var cap in caps) {
+    context.drawImage(caps[cap], x, y, 50, 50);
+    x += 53;
+    if (x >= width) {
+      y += 53;
+      x = 20;
+    }
   }
 }
 
